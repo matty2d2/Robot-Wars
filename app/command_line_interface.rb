b2c8c780ad1a965af8bbc979ef2eeea28a26a516
github username <github email address>
2019-09-11 17:19:52 +0100

    added change name (update)

--- app/command_line_interface.rb
+++ app/command_line_interface.rb
@@ -116,8 +116,11 @@ def my_robots
         sleep(1)
         $robot = myrobot
 
-        choice = prompt.select("", "Fight with this Robot!", "Return to my Robots")
-        if choice == "Fight with this Robot!"
+        choice = prompt.select("", "Fight with this Robot!", "Configure Robot", "Return to my Robots")
+        if choice == "Configure Robot"
+            configure_robot
+            my_robots
+        elsif choice == "Fight with this Robot!"
             choose_gamemode
         elsif choice == "Return to my Robots"
             my_robots
@@ -192,9 +195,7 @@ def create_a_robot
         puts myrobot.attributes.reject{|k,v| k == "id" || k == "player_id" || k == "name"}
     else
         puts "Robot has not been created. There is already a robot with that name."
-        player_menu
     end
-    
 end
 
 def valid_robot_name?(name)
@@ -222,4 +223,30 @@ def stats
     sleep(0.5)
     prompt.ask("Press Enter to return back to the player menu.")
     player_menu
+end
+########################################
+########################################
+def configure_robot
+    prompt = TTY::Prompt.new
+    choice = prompt.select("What would you like to configure?", "Change Robot Name", "Back to Player Menu", "Quit Game")
+
+    if choice == "Change Robot Name"
+        change_name
+    elsif choice == "Back to Player Menu"
+        player_menu
+    elsif choice == "Quit Game"
+        quit_game
+        return
+    end
+end
+
+def change_name
+    prompt = TTY::Prompt.new
+    rob_name = prompt.ask("What would you like to change your robot name to:", required: true)
+
+    if valid_robot_name?(rob_name)
+        $robot.update(name: rob_name)
+    else
+        puts "Robot name has not been changed. There is already a robot with that name."
+    end
 end
