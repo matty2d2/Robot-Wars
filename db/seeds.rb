d5e71fc00d3ff79678e64e8003d33ae365cacb3a
github username <github email address>
2019-09-13 13:00:40 +0100

    added attack (doesnt do anything though)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
     Player.create(username: Faker::Name.unique.first_name)
 end
 
-80.times do
-    Robot.create(name: Faker::Games::Pokemon.unique.name, player_id: rand(1..10))
+90.times do
+    Robot.create(name: Faker::Games::Pokemon.unique.name, player_id: rand(1..10), attack: rand(5..9))
 end
 
 
